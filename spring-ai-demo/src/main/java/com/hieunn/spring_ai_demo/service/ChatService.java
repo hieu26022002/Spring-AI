6161a9083433154ad89859dbfbc6a3d280a61182
unknown <nguyenngochieu26022002@gmail.com>
2025-07-04 14:37:01 +0700

    update prompt

--- spring-ai-demo/src/main/java/com/hieunn/spring_ai_demo/service/ChatService.java
+++ spring-ai-demo/src/main/java/com/hieunn/spring_ai_demo/service/ChatService.java
@@ -3,6 +3,9 @@ package com.hieunn.spring_ai_demo.service;
 import com.hieunn.spring_ai_demo.mode.ChatRequest;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,16 @@ public class ChatService {
     }
 
     public String chat(ChatRequest chatRequest) {
+        SystemMessage systemMessage = new SystemMessage("""
+                You are HieuNN.AI
+                """);
+        UserMessage userMessage = new UserMessage(chatRequest.message());
+
+        // Prompt điều chỉnh AI phục vụ theo mục đích của mình
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
         return chatClient
-                .prompt(chatRequest.message())
+                .prompt(prompt)
                 .call()
                 .content();
     }
